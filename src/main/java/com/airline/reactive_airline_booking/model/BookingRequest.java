29a08bf92ea58911b3cbf655ea941358cea623cf
rinki-2022 <rinkibansal2022@gmail.com>
2026-01-12 06:43:41 -0500

    Added class Booking Request

--- src/main/java/com/airline/reactive_airline_booking/model/BookingRequest.java
+++ src/main/java/com/airline/reactive_airline_booking/model/BookingRequest.java
@@ -1,4 +1,12 @@
 package com.airline.reactive_airline_booking.model;
 
+import lombok.Data;
+
+@Data
 public class BookingRequest {
+    private String flightId;
+    private String passengerId;
+    private String passengerName;
+    private String passengerEmail;
+    private String seatPreference;
 }
